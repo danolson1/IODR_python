bcb7ac343962c37dfd11609844c2ed13f53a7e1c
Daniel Olson <dan268@gmail.com>
2020-05-19 17:04:26 -0400

    IODR_data update for objects
    I separated the instance methods from the class methods

--- IODR_data.py
+++ IODR_data.py
@@ -31,23 +31,18 @@ local_tz = pytz.timezone('US/Eastern')
 # set up logging
 logger = logging.getLogger(__name__) # ensure log messages have correct module name
 
-class IODR:
-    def __init__(self, device_number):
-        self.device_number = device_number
-        self.chID = chIDs[device_number-1]
-        self.readAPIkey = readAPIkeys[device_number-1]
+# helper methods are outside of the IODR class
+def local_to_UTC(date_string):
+    """
+    take a local date/time string and convert to UTC date/time string
+    helper function for get_data()
+    """
+    local_dt = dt.datetime.strptime(date_string, "%Y-%m-%d %H:%M").astimezone(local_tz)
+    utc_dt = local_dt.astimezone(pytz.utc)
+    return utc_dt.strftime("%Y-%m-%d %H:%M")
 
-    def local_to_UTC(date_string):
-        """
-        take a local date/time string and convert to UTC date/time string
-        helper function for thingspeak_to_df()
-        """
-        local_dt = dt.datetime.strptime(date_string, "%Y-%m-%d %H:%M").astimezone(local_tz)
-        utc_dt = local_dt.astimezone(pytz.utc)
-        return utc_dt.strftime("%Y-%m-%d %H:%M")
-    
-    
-    def got_all_data(start_date, dataframe):
+
+def got_all_data(start_date, dataframe):
         """
         Check to see if the dataframe begins at the requested start date
         This is useful to see if all of the requested data is actually in the dataframe
@@ -60,56 +55,62 @@ class IODR:
             return False
         else:
             return True
+
+
+def get_data(chID, start_date, end_date = dt.datetime.now().strftime("%Y-%m-%d %H:%M")):
+    """
+    Get data from Thingspeak, single request
+    Return as a dataframe with time in UTC timezone
+    If no end date is provided, use the current time as the end date
+    Thinkspeak can only get 8000 datapoints per request. If more datapoints are requested, Thingspeak
+      returns the most recent 8000 points. To get all of the data, you need to do a second request to 
+      get the earlier data
+    """
+    start_date_UTC = local_to_UTC(start_date)
+    end_date_UTC = local_to_UTC(end_date)
+    logger.info('getting data from {start} to {end}, in UTC timezone'.format(start = start_date_UTC, end = end_date_UTC))
+    myUrl = 'https://api.thingspeak.com/channels/{channel_id}/feeds.csv?start={start}&end={end}'.format(
+        channel_id = chID, start = start_date_UTC, end = end_date_UTC)
+    logger.debug(myUrl)
+    r = requests.get(myUrl)
+    logger.debug('First 200 characters of HTTP request, for troubleshooting')
+    logger.debug(r.content.decode('utf-8')[0:200])
     
+    # put the thingspeak data in a dataframe
+    df = pd.read_csv(io.StringIO(r.content.decode('utf-8')))
+
+
+    df2 = df.copy()
+    if 'entry_id' in df2.columns:
+        df2.drop('entry_id', axis = 'columns', inplace = True)
     
-    def thingspeak_to_df(start_date, end_date = dt.datetime.now().strftime("%Y-%m-%d %H:%M")):
+    df2['time'] = pd.to_datetime(df2['created_at']).dt.tz_convert('UTC') # convert UTC to eastern time
+    df2 = df2.drop('created_at', axis = 'columns')
+    df2.set_index('time', inplace = True)
+
+    return df2
+
+# The IODR class
+class IODR:
+    def __init__(self, device_number):
+        self.device_number = device_number
+        self.chID = chIDs[device_number-1]
+        self.readAPIkey = readAPIkeys[device_number-1]
+  
+        
+    def get_all_data(self, start_date, end_date):
         """
-        Get data from Thingspeak, single request
-        Return as a dataframe with time in UTC timezone
-        If no end date is provided, use the current time as the end date
-        Thinkspeak can only get 8000 datapoints per request. If more datapoints are requested, Thingspeak
-          returns the most recent 8000 points. To get all of the data, you need to do a second request to 
-          get the earlier data
-        """
-        
-        start_date_UTC = local_to_UTC(start_date)
-        end_date_UTC = local_to_UTC(end_date)
-        logger.info('getting data from {start} to {end}, in UTC timezone'.format(start = start_date_UTC, end = end_date_UTC))
-        myUrl = 'https://api.thingspeak.com/channels/{channel_id}/feeds.csv?start={start}&end={end}'.format(
-            channel_id = self.chID, start = start_date_UTC, end = end_date_UTC)
-        logger.debug(myUrl)
-        r = requests.get(myUrl)
-        logger.debug('First 200 characters of HTTP request, for troubleshooting')
-        logger.debug(r.content.decode('utf-8')[0:200])
-        
-        # put the thingspeak data in a dataframe
-        df = pd.read_csv(io.StringIO(r.content.decode('utf-8')))
-    
-    
-        df2 = df.copy()
-        if 'entry_id' in df2.columns:
-            df2.drop('entry_id', axis = 'columns', inplace = True)
-        
-        df2['time'] = pd.to_datetime(df2['created_at']).dt.tz_convert('UTC') # convert UTC to eastern time
-        df2 = df2.drop('created_at', axis = 'columns')
-        df2.set_index('time', inplace = True)
-    
-        return df2
-        
-        
-    def get_all_data(start_date, end_date):
-        """
-        Get all requested data from Thingspeak, making multiple requests if needed
+        Get all requested data from Thingspeak, for a specific IODR object, making multiple requests if needed
         Return the results as a Pandas DataFrame
         """
     
         # perform the request to get data from thingspeak
-        df = thingspeak_to_df(start_date, end_date)
+        df = get_data(self.chID, start_date, end_date)
         
         # if all of the data wasn't collected, because it was more than 8000 points, do another request
         while (got_all_data(start_date, df) is False):
             print('getting more data...')
-            df_extra = thingspeak_to_df(start_date, df.index[0].strftime("%Y-%m-%d %H:%M"))
+            df_extra = get_data(self.chID, start_date, df.index[0].strftime("%Y-%m-%d %H:%M"))
             df = pd.concat([df, df_extra]).drop_duplicates().sort_index()
             
         df2 = df.copy()
